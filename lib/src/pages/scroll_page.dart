9b927c38a6b883ed0839765cd1cb60a0d2e5f41e
David Lodi <lodi.dev@outlook.com>
2019-10-31 23:51:41 -0600

    fin del segundo ejercicio

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -14,14 +14,69 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pag 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+    final estiloTxt = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Text('11', style:estiloTxt),
+          Text('Miércoles', style:estiloTxt),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
     );
   }
 
   Widget _pagina2() {
-    return Center(
-      child: Text('Pag 2'),
+    return Stack(
+     children: <Widget>[
+       _colorFondo(),
+       Center(
+         child: RaisedButton(
+           child: Padding(
+             padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+             child: Text('Bienvenidos', style: TextStyle(fontSize: 20.0)),
+           ),
+           onPressed: (){},
+           shape: StadiumBorder(),
+           color: Colors.blue,
+           textColor: Colors.white,
+         ),
+       )
+     ],
     );
   }
 }
